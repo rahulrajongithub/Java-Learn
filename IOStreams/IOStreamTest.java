4aa811beb9ccf6493ca70c3099b78240fcfc6141
Rahul Raj <themailtorahul@gmail.com>
2021-05-30 16:43:13 +0530

    Added IO stream and JSON files. But not working whatsoever

---
+++ IOStreams/IOStreamTest.java
@@ -0,0 +1,19 @@
+package IOStreams;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class IOStreamTest {
+    public static void main(String[] args) throws IOException {
+        String path = "C:\\Users\\USER\\Desktop\\Coding\\JavaCodes\\Java-Learn\\IOStreams";
+        InputStream input = new FileInputStream(path);
+        int data = input.read();
+        while(data != -1){
+            data = input.read();
+            System.out.println(data);
+        }
+    }
+    
+}
---
+++ JSONPrograms/JSONTestEncoding.java
@@ -0,0 +1,9 @@
+package JSONPrograms;
+
+import org.json.simple.JSONObject;
+
+public class JSONTestEncoding {
+    public static void main(String[] args) {
+        JSONObject file = new JSONObject();
+    }
+}
