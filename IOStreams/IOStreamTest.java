5ca5c64182258e61ac37d9439d988e550f96d16d
Rahul Raj <themailtorahul@gmail.com>
2021-06-04 20:22:42 +0530

    JDBC is working now

--- IOStreams/IOStreamTest.java
+++ IOStreams/IOStreamTest.java
@@ -1,8 +1,6 @@
 package IOStreams;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.io.InputStream;
 
 public class IOStreamTest {
---
+++ RoughWorkout/Testjava.java
@@ -0,0 +1,9 @@
+package RoughWorkout;
+public class Testjava {
+    public static void main(String[] args) {
+        int y = 2025;
+        String a = "Hi ";
+        String z = a + y;
+        System.out.println(z);
+    }
+}
